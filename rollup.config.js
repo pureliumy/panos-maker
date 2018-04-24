e29a92aa1cd680719a4f224f5aab827bec48a05c
Liumy <alephantand@gmail.com>
2018-04-24 11:46:59 +0800

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'build/index.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'build/index.es.js',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      presets: [
+        [
+          'env',
+          {
+            modules: false
+          }
+        ]
+      ],
+      plugins: ['external-helpers'],
+      babelrc: false
+    })
+  ]
+}
