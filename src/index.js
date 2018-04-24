70258a47a07f1810eb5e0f2fc780e7a31610d695
Liumy <alephantand@gmail.com>
2018-04-24 19:01:30 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ const commander = require('commander')
 
 commander
   .version('0.0.1')
+  .usage('[options] <file ...>')
   .option('-p, --pano', 'make panos by normal config')
   .option('-t, --tour', 'make tours by normal config')
   .option('-f, --flat', 'make panos by flat config, only work with --pano')
   .option('-m, --multires', 'make panos/tours by multires config')
   .option('-v, --vr', 'make tours by vr config, only word with --tour')
+  .option('-c, --config <config file>', 'specify your custom config')
   .parse(process.argv)
+
+
+const type = (commander.pano || !commander.tour) ? 'pano' : 'tour'
+
+const defaultConfig = `${type}-`
+
+const config = commander.config || defaultConfig
